662ca6bd4666ac1ea88991e534ec9e080138b30c
dalvaradotey <d.alvaradotey@gmail.com>
2021-01-25 20:10:37 -0300

    UPDATE: información de prints

--- src/components/intro.js
+++ src/components/intro.js
@@ -11,21 +11,8 @@ const Intro = () => (
         <li>Impresión Fine Art por Gronefot</li>
         <li>Papel de Algodón Canson® Infinity Edition Etching Rag 310g/m</li>
         <li>Prints intervenidos a mano con marcadores acrílicos Molotow™ One4all™</li>
-        <li>Enmarcado en madera negra lacada con vidrio por Amazonas</li>
+        <li>Enmarcado en madera negra lacada con vidrio</li>
       </ul>
-      <div className="pv1 tracked">---</div>
-    </div>
-    <div>
-      <h2 className="b">Valores Prints enmarcados</h2>
-      <ul className="lh-copy">
-        <li>60 x 40 cm $135.000 CLP</li>
-        <li>45 x 30 cm $110.000 CLP</li>
-        <li>30 x 20 cm $65.000 CLP</li>
-      </ul>
-    </div>
-    <div>
-      <div className="pv1 tracked">---</div>
-      <p className="b">Aprovecha un 30% de descuento por lanzamiento!</p>
       <div className="pv1 tracked">---</div>
     </div>
     <div>
